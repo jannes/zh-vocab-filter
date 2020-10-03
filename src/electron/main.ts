1d891cde7f64c277d1b788b3dda2f2e1331a6f97
jannes <jannes.dev@posteo.net>
2020-10-03 23:17:05 +0200

    split electron sources in extra folder

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -92,7 +92,7 @@ function createWindow(): BrowserWindow {
 
     require('electron-reload')(__dirname, {
       electron: require(
-        `${__dirname}/node_modules/electron`
+        `${__dirname}/../node_modules/electron`
       )
     });
     win.loadURL('http://localhost:4200');
